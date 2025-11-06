f7e535629cb4f6acd1b4d7ae97546f57270466ab
마리임 <78864678+mjkim0727@users.noreply.github.com>
2025-11-07 00:45:02 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-echo "Installing Quantum-icon-theme..."
+echo "Installing Pure-icon-theme..."
 
-cp -r ../src/Pure* ~/.local/share/icons
+cp -r src/Pure* ~/.local/share/icons
+gtk-update-icon-cache -f ~/.local/share/icons/Pure*
 
 exit 0
